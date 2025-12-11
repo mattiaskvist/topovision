1a564c740529de0e969c8c58aec23fd3a5220dd1
Mattias Kvist <makv@kth.se>
2025-12-11 02:17:11 +0100

    feat: add tests to ci

---
+++ tests/test_placeholder.py
@@ -0,0 +1,6 @@
+"""This is a placeholder test file to ensure pytest runs successfully."""
+
+
+def test_placeholder():
+    """A placeholder test to ensure pytest runs successfully."""
+    assert True
